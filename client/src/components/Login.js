8be24a1805b1f0abdb7063999652b35d59cc0a1d
sheridan <sheridanmaustad@gmail.com>
2022-09-01 12:18:35 -0600

    Adding styles and components

---
+++ client/src/components/Login.js
@@ -0,0 +1,80 @@
+import {
+	Label,
+	FormField,
+	Input,
+	Error,
+	Button,
+	Container,
+	Wrapper,
+	Title,
+	Form,
+} from '../styles';
+import { useState } from 'react';
+
+const Login = ({onLogin}) => {
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+	const [errors, setErrors] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		setIsLoading(true)
+		fetch('/api/login', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({username, password})
+		}).then((r) => {
+			setIsLoading(false)
+			if (r.ok) {
+				r.json().then((user) => {
+					onLogin(user)
+				})
+			} else {
+				r.json().then((err) => setErrors(err.errors))
+			}
+		})
+	}
+
+	return (
+		<Container>
+			<Wrapper>
+				<Title>PLEASE SIGN IN</Title>
+				<Form onSubmit={handleSubmit}>
+				<FormField>
+					<Label htmlFor='username'>Username</Label>
+					<Input
+						type='text'
+						id='username'
+						value={username}
+						onChange={(e) => setUsername(e.target.value)}
+					/>
+				</FormField>
+				<FormField>
+					<Label htmlFor='password'>Password</Label>
+					<Input
+						type='password'
+						id='password'
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+				</FormField>
+				<FormField>
+					<Button variant='fill' color='primary' type='submit'>
+						{isLoading ? 'Loading...' : 'Login'}
+					</Button>
+				</FormField>
+				<FormField>
+					{errors.map((err) => (
+						<Error key={err}>{err}</Error>
+					))}
+				</FormField>
+				</Form>
+			</Wrapper>
+		</Container>
+	);
+};
+
+export default Login;
