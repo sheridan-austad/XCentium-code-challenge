236d40b4d34b9eb3e1dd20a0740e26ed0541a838
sheridan <sheridanmaustad@gmail.com>
2022-09-01 14:19:11 -0600

    Attempting to add a hero banner

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,26 +1,25 @@
 import React from "react";
-import { Title, Container, Box } from "../styles";
-import "../index.css";
+import { Title, Box } from "../styles";
+// import "../index.css";
 
 const Home = ({ user }) => {
   return (
-    <>
-      <Box>
-        <Container>
-          <img
-            src="/XCentium_hero_banner.png"
-            alt="hero banner"
-            className="hero"
-            // styles={{ maxWidth: "1024px", height: "100px" }}
-          />
-        </Container>
-      </Box>
-      <Box>
-        <Container>
+    <Box>
+      <div
+        className="hero-image"
+        style={{ 
+          backgroundImage: "url(/'https://www.pixelstalk.net/wp-content/uploads/2016/06/Technology-Wallpaper.jpg')",
+          // height: 100%,
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+          position: 'relative',
+        }}
+      >
           <Title>Welcome Back {user.name}!</Title>
-        </Container>
-      </Box>
-    </>
+
+      </div>
+    </Box>
   );
 };
 
