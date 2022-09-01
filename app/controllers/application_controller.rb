002686a291a4c72ed17a69478975a20002712a91
sheridan <sheridanmaustad@gmail.com>
2022-09-01 11:52:45 -0600

    Setting up backend

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,26 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
+    include ActionController::Cookies
+  
+    rescue_from ActiveRecord::RecordNotFound, with: :not_found
+    rescue_from ActiveRecord::RecordInvalid, with: :not_processed
+  
+    before_action :authorize
+  
+    private
+  
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  
+    def authorize
+      render json: { errors: ["not authorize"]}, status: :unauthorized unless current_user
+    end
+  
+    def not_found(exception)
+      render json: { errors: ["#{exception.model} not found"]}, status: :not_found
+    end
+  
+    def not_processed(invalid)
+      render json: { errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    has_secure_password
+
+    validates :name, presence: true, uniqueness: true
+    validates :username, presence: true, length: {in: 3..25}
+    validates :password, length: {in: 5..25}
+    
 end
